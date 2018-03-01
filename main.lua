b0882881d32451734ac0b9fb1e2f63262b713d15
Nice Mena <nice.mena@ocsbstudent.ca>
2018-03-01 10:46:26 -0500

    Initial commit

---
+++ main.lua
@@ -0,0 +1,10 @@
+-----------------------------------------------------------------------------------------
+---- Title: MovingObjects
+-- Name: Nice Mena
+-- Course: ICS2O/3C
+-- This program moves a beetleship moving
+--across the screenas well as another
+--object moving in a different direction.
+
+
+print ("****Testing Bettleship!")
